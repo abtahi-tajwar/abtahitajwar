d6f2ca25dc1c0d57731fd752785b7e38c9da6bf3
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-06-23 21:09:23 +0600

    >>>>>>

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', (e)=> {
 
     let items = document.querySelectorAll(".item");
     items.forEach(item => {
-        if(window.scrollY+window.innerHeight > item.offsetTop+20 ) {
+        if(window.scrollY+window.innerHeight > item.offsetTop ) {
             item.style.animation = "itemsFadeIn 1s ease-out forwards";
         }
         else {
@@ -24,4 +24,37 @@ document.querySelector(".selectMenuHeading").addEventListener('click', () => {
         trayOpen = 0;
     }
     
-})
+});
+document.querySelectorAll(".selectMenu ul li").forEach(item => {
+    item.addEventListener('click', (e) => {
+        let tag = e.target.attributes['tag'].value;
+        if(tag === 'websites') {
+            document.querySelector(".websites").style.display = "block";            
+            document.querySelector(".item").style.display = "none";
+            document.querySelector(".components").style.display = "none";
+
+            document.querySelector(".selectMenu ul li:nth-child(1)").classList.add("selected");
+            document.querySelector(".selectMenu ul li:nth-child(2)").classList.remove("selected");
+            document.querySelector(".selectMenu ul li:nth-child(3)").classList.remove("selected");
+        }
+        else if(tag === 'components') {
+            document.querySelector(".websites").style.display = "none";
+            document.querySelector(".item").style.display = "block";
+            document.querySelector(".components").style.display = "none";
+
+            document.querySelector(".selectMenu ul li:nth-child(1)").classList.remove("selected");
+            document.querySelector(".selectMenu ul li:nth-child(2)").classList.add("selected");
+            document.querySelector(".selectMenu ul li:nth-child(3)").classList.remove("selected");
+        }
+        else if(tag === 'standalone') {
+            document.querySelector(".websites").style.display = "none";
+            document.querySelector(".item").style.display = "none";
+            document.querySelector(".components").style.display = "block";
+
+            document.querySelector(".selectMenu ul li:nth-child(1)").classList.remove("selected");
+            document.querySelector(".selectMenu ul li:nth-child(2)").classList.remove("selected");
+            document.querySelector(".selectMenu ul li:nth-child(3)").classList.add("selected");
+            
+        }
+    })  
+});
