a50893b4325988609686ed971b6a8152ef37144b
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-06-23 20:37:53 +0600

    Some issues fixed

--- home.js
+++ home.js
@@ -4,12 +4,24 @@ window.addEventListener('scroll', (e)=> {
 
     let items = document.querySelectorAll(".item");
     items.forEach(item => {
-        if(window.scrollY+window.innerHeight > item.offsetTop ) {
+        if(window.scrollY+window.innerHeight > item.offsetTop+20 ) {
             item.style.animation = "itemsFadeIn 1s ease-out forwards";
         }
         else {
             item.style.animation = "none";
         }
     })
+    
+})
+let trayOpen = 0;
+document.querySelector(".selectMenuHeading").addEventListener('click', () => {
+    if(trayOpen === 0) {
+        document.querySelector(".selectMenu").style.height = "200px";
+        trayOpen = 1;
+    }
+    else {
+        document.querySelector(".selectMenu").style.height = "50px"
+        trayOpen = 0;
+    }
     
 })
